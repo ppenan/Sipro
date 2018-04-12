ea239ebc4ebbb0428a327b8a2b05a0fb427c6795
ppenan <pedro_penan@hotmail.com>
2018-04-12 08:49:49 -0600

    Agregar archivos de proyecto.

---
+++ SiproNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SiproNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
